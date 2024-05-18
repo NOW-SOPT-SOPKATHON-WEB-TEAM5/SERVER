f2233dde5e7785d55f91313809116890904dd5d2
elive7 <parksub020704@gmail.com>
2024-05-19 04:15:52 +0900

    fix: 최종 리스트 조회 수정

--- src/main/java/sopt/hackathon/hakka/dto/response/FinalWishResponseDto.java
+++ src/main/java/sopt/hackathon/hakka/dto/response/FinalWishResponseDto.java
@@ -3,13 +3,13 @@ package sopt.hackathon.hakka.dto.response;
 import java.util.List;
 import sopt.hackathon.hakka.domain.FinalWish;
 
-public record FinalWishResponseDto(Long wishId, String content) {
+public record FinalWishResponseDto(Long wishId, Long questionId, String content) {
     public static List<FinalWishResponseDto> findAll(List<FinalWish> finalWishList){
         List<FinalWishResponseDto> list = finalWishList.stream().map(FinalWishResponseDto::of).toList();
         return list;
     }
 
     public static FinalWishResponseDto of (FinalWish finalWish){
-        return new FinalWishResponseDto(finalWish.getWish().getWishId(), finalWish.getWish().getContent());
+        return new FinalWishResponseDto(finalWish.getWish().getWishId(), finalWish.getWish().getQuestion().getQuestionId(),finalWish.getWish().getContent());
     }
 }
